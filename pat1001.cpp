bf1ecde04c99a835f62536bf4e23b745ed50f72d
changed <1145497359@qq.com>
2016-08-04 17:34:00 +0800

    pat1001

---
+++ pat1001.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <stdio.h>
+using namespace std;
+int main()
+{
+	int n;
+	while(scanf("%d",&n)!=EOF)
+	{
+		int i=0;
+		while(n!=1)
+		{
+			if(n%2==0)
+			{
+				n=n/2;
+			}else
+			{
+				n=3*n+1;
+				n=n/2;
+			}
+			i++;
+		}
+	cout<<i<<endl;		
+	}
+	return 0;
+} 
